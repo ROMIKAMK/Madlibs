f3b6eb1894414040ca4f269d89f7073c107910ad
ROMIKAMK <31789149+ROMIKAMK@users.noreply.github.com>
2022-06-30 21:40:52 +0530

    Add files via upload

---
+++ 1_madlibs.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Hp
+#
+# Created:     23-06-2022
+# Copyright:   (c) Hp 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#string concatenation
+adj = input("Adjective: ")
+verb1 = input("Verb:")
+verb2 = input("Verb:")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the \
+time because I love to {verb1}. Stay hydrated and {verb2} like you are  \
+{famous_person}! "
+
+print(madlib)
+
